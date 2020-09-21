cc004208486150df12a395045f63d5b38f908fba
adcash-pa <eugene.adcash@gmail.com>
2020-09-21 18:47:04 +0300

    draft for dynamic close, gap update

--- draft.js
+++ draft.js
@@ -14,23 +14,32 @@ function closeNote() {
   for(var i = 0, len = closeArray.length; i < len; i++) {
       console.log(noteArray[i].id);
       noteArray[i].onclick = function() {
-      this.setAttribute('style','display:none');
+        this.setAttribute('style','display:none');
+        //gapCalc();
+        console.log(this);
     }
   }
 }
 
-function gapCalc() {
+function gapCalc(skip) {
   var container  = document.querySelector("#frame");
       matches    = container.querySelectorAll("div[id*='note-']");
       wWidth     = window.innerWidth;
       gap        = 0;
+      skipV      = [0];
+      skipV.push(skip);
+      console.log(skipV);
 
       if (wWidth < 480) {
         gap = 18;
       }
 
   for (i = 0; i < matches.length; i++) {
-    if (i > 0) {
+    if (skipV.includes(i)) {
+      continue;
+    }
+    else
+    {
       var offestValue = parseInt(matches[i-1].offsetTop) +   parseInt(matches[i-  1].offsetHeight) + gap;
       matches[i].setAttribute('style','top:'+ offestValue + 'px');
     }
