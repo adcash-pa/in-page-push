4ef196db912add24d5a7e15e1b807962a39d904c
adcash-pa <eugene.adcash@gmail.com>
2020-09-21 22:20:03 +0300

    fix close function

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.onload  = function() {
+window.onload = function() {
   gapCalc();
   closeNote();
 }
@@ -7,32 +7,33 @@ window.onresize = function() {
 }
 
 function closeNote() {
-  var container  = document.querySelector("#frame");
-      closeArray = container.querySelectorAll("div[id*='close-']");
-      noteArray  = container.querySelectorAll("div[id*='note-']");
+  var container = document.querySelector("#frame");
+  closeArray = container.querySelectorAll("div[id*='close-']");
+  noteArray = container.querySelectorAll("div[id*='note-']");
 
-  for(var i = 0, len = closeArray.length; i < len; i++) {
-      //console.log(noteArray[i].id);
-      noteArray[i].onclick = function() {
-      this.setAttribute('style','display:none');
-    }
+  for (var i = 0, len = closeArray.length; i < len; i++) {
+    (function(index) {
+      closeArray[index].onclick = function() {
+        noteArray[index].setAttribute('style', 'display:none');
+      }
+    })(i);
   }
 }
 
 function gapCalc() {
-  var container  = document.querySelector("#frame");
-      matches    = container.querySelectorAll("div[id*='note-']");
-      wWidth     = window.innerWidth;
-      gap        = 0;
+  var container = document.querySelector("#frame");
+  matches = container.querySelectorAll("div[id*='note-']");
+  wWidth = window.innerWidth;
+  gap = 0;
 
-      if (wWidth < 480) {
-        gap = 18;
-      }
+  if (wWidth < 480) {
+    gap = 18;
+  }
 
   for (i = 0; i < matches.length; i++) {
     if (i > 0) {
-      var offestValue = parseInt(matches[i-1].offsetTop) +   parseInt(matches[i-  1].offsetHeight) + gap;
-      matches[i].setAttribute('style','top:'+ offestValue + 'px');
+      var offestValue = parseInt(matches[i - 1].offsetTop) + parseInt(matches[i - 1].offsetHeight) + gap;
+      matches[i].setAttribute('style', 'top:' + offestValue + 'px');
     }
   }
 }
